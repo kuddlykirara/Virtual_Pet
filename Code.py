a2f35c319b755285033678c7dda2fbfba6df4b8b
kuddlykirara <64767602+kuddlykirara@users.noreply.github.com>
2022-12-26 02:07:52 -0600

    Code
    Python code for virtual pet

---
+++ Code.py
@@ -0,0 +1,44 @@
+class VirtualPet:
+  def __init__(self, name, image, sound):
+    self.name = name
+    self.image = image
+    self.sound = sound
+    self.hunger = 0
+    self.thirst = 0
+    self.health = 100
+  
+  def feed(self):
+    if self.hunger > 0:
+      self.hunger -= 10
+      print("Yum, thank you for feeding me!")
+    else:
+      print("I'm not hungry right now.")
+  
+  def water(self):
+    if self.thirst > 0:
+      self.thirst -= 10
+      print("Ahh, thank you for giving me something to drink!")
+    else:
+      print("I'm not thirsty right now.")
+  
+  def pet(self):
+    print("Purr, thank you for petting me!")
+  
+  def give_medicine(self):
+    if self.health < 100:
+      self.health += 10
+      print("Thank you for giving me medicine. I feel better now!")
+    else:
+      print("I'm already feeling healthy.")
+  
+  def save_state(self):
+    # save the pet's state to a file
+    pass
+
+# create a virtual pet and interact with it
+pet = VirtualPet("Fairy", "cat.png", "meow.mp4")
+pet.feed()
+pet.water()
+pet.pet()
+pet.give_medicine()
+pet.save_state()
